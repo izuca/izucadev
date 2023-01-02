07dbeb705784ab82c0fd160348424213529c8113
adriel.santos <adriel200010@hotmail.com>
2023-01-02 16:55:35 -0300

    Projetos feito

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,25 @@
 const postcss = require('postcss');
 const tailwindcss = require('tailwindcss');
 const autoprefixer = require('autoprefixer');
+const Image = require("@11ty/eleventy-img");
 
+async function imageShortcode(src, alt, sizes) {
+  let metadata = await Image(`./src${src}`, {
+    widths: [300, 800, null],
+    formats: ["avif", "jpeg"],
+    urlPath: "/images/",
+    outputDir: "./public/images/"
+  });
+
+  let imageAttributes = {
+    alt,
+    sizes,
+    loading: "lazy",
+    decoding: "async"
+  };
+
+  return Image.generateHTML(metadata, imageAttributes);
+}
 module.exports = (eleventyConfig) => {
   eleventyConfig.addNunjucksAsyncFilter('postcss', (cssCode, done) => {
     postcss([tailwindcss(require('./tailwind.config.js')), autoprefixer()])
@@ -14,6 +32,7 @@ module.exports = (eleventyConfig) => {
   eleventyConfig.addPassthroughCopy("./src/images/");
   eleventyConfig.addPassthroughCopy({ "./src/favicons": "/" }); 
   eleventyConfig.addWatchTarget('styles/**/*.css');
+  eleventyConfig.addNunjucksAsyncShortcode("EleventyImage", imageShortcode);
   return {
     dir: {
       input: 'src',
