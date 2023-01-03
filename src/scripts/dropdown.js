be74a92f265f773e5f21b0785e136db37de23a4c
adriel.santos <adriel200010@hotmail.com>
2023-01-03 04:51:07 -0300

    responsividade

--- src/scripts/dropdown.js
+++ src/scripts/dropdown.js
@@ -2,5 +2,21 @@ const btn = document.getElementById("dropdown")
 
 btn.addEventListener("click", ()=>{
     console.log("dropdown")
-    
+    if(document.getElementById("menu").classList.contains("hidden")){
+        document.getElementById("menu").classList.remove("hidden")
+        document.getElementById("menu").classList.add("lg:hidden")
+        document.getElementById("menu").classList.add("flex")
+    } else{
+        document.getElementById("menu").classList.remove("flex")
+        document.getElementById("menu").classList.remove("lg:hidden")
+        document.getElementById("menu").classList.add("hidden")
+    }
+})
+
+document.getElementById("menu").addEventListener("click", ()=> {
+    if(document.URL!= "http://localhost:8080/" && document.getElementById("menu").classList.contains("flex")){
+        document.getElementById("menu").classList.remove("flex")
+        document.getElementById("menu").classList.remove("lg:hidden")
+        document.getElementById("menu").classList.add("hidden")
+    }
 })
